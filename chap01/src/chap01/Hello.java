05f62b87d6f01cdea9e212f3be53f52cfb62b74e
rg <lustellz@gmail.com>
2020-02-15 23:03:15 +0900

    [2020.02.15] StudyDay1

---
+++ chap01/src/chap01/Hello.java
@@ -0,0 +1,10 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Java."); //println: 문자열 출력 명령어
+	}
+
+}
