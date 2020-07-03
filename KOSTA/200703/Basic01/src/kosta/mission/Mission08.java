aa01a35883a14e9278a2b0aae67a685cbb0a9746
rg <lustell@gmail.com>
2020-07-03 17:47:06 +0900

    [2020.07.03] Array

---
+++ KOSTA/200703/Basic01/src/kosta/mission/Mission08.java
@@ -0,0 +1,12 @@
+package kosta.mission;
+
+public class Mission08 {
+
+	public static void main(String[] args) {
+		// 메뉴를 선택해서 해당 메뉴의 명령을 실행하세요.
+		
+		//String 배열을 생성해서 그 요소에 대한 작업을 합니다.
+		//1. 추가 2. 출력 3. 검색(해당 인덱스의 배열 값 출력) 4. 종료
+	}
+
+}
